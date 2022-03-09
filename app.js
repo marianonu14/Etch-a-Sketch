0565bdf8e663cdc7d7a35be059b2aee98409b90b
marianonu14 <marianonunezma@gmail.com>
2022-03-09 19:17:19 -0300

    First Steps with JS

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const btnColor = document.querySelector('#color');
+const contenedorPrincipal = document.querySelector('#contenedor');
 const firstGrid = document.querySelector('#first-grid');
 const secondGrid = document.querySelector('#second-grid');
 const thirdGrid = document.querySelector('#third-grid');
 const resetGrid = document.querySelector('#reset');
+let selectColor = '#000000';
 
-btnColor.addEventListener('click', () => {
-    selectColor();
+btnColor.addEventListener('change', (e) => {
+    selectColor = e.target.value;
   });
 
-  function selectColor() {
-      console.log('click');
+contenedorPrincipal.addEventListener('mousemove', () => {
+    contenedorPrincipal.style.backgroundColor = selectColor;
+    contenedorPrincipal.style.transition = '200ms';
+  });
+
+  firstGrid.addEventListener('click', () => {
+    createDiv();
+  });
+
+resetGrid.addEventListener('click', () => {
+    contenedorPrincipal.style.backgroundColor = '#FFFFFF';
+    btnColor.value = '#000000';
+    selectColor = '#000000';
+  });
+
+  function createDiv() {
+    contenedorPrincipal.createElement("DIV");  
   };
+
+
+
