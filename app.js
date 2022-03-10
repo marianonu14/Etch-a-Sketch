045d6fb2cc7d91d40618ce62bd1d86ee977cbc18
marianonu14 <marianonunezma@gmail.com>
2022-03-09 22:52:17 -0300

    Create Div Function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const firstGrid = document.querySelector('#first-grid');
 const secondGrid = document.querySelector('#second-grid');
 const thirdGrid = document.querySelector('#third-grid');
 const resetGrid = document.querySelector('#reset');
+const newDiv = document.createElement("div");
+newDiv.textContent = "Esto es un DIV"
+
 let selectColor = '#000000';
 
 btnColor.addEventListener('change', (e) => {
@@ -15,8 +18,8 @@ contenedorPrincipal.addEventListener('mousemove', () => {
     contenedorPrincipal.style.transition = '200ms';
   });
 
-  firstGrid.addEventListener('click', () => {
-    createDiv();
+firstGrid.addEventListener('click', () => {
+    createDiv(firstGrid, 100);
   });
 
 resetGrid.addEventListener('click', () => {
@@ -24,10 +27,12 @@ resetGrid.addEventListener('click', () => {
     btnColor.value = '#000000';
     selectColor = '#000000';
   });
-
-  function createDiv() {
-    contenedorPrincipal.createElement("DIV");  
-  };
-
-
-
+  
+function createDiv(grid, cant) {
+    for (let step = 0; step < cant; step++) {
+    //contenedorPrincipal.appendChild(newDiv);
+    console.log('Creando Div');
+    grid.disabled = true;
+    grid.classList.add('active')
+   };
+};
